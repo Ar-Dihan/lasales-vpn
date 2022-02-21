380a08f2e4b4419c58b968f2ce829ef6a24dd6b0
Ar-Dihan <arnahid18@gmail.com>
2022-02-21 21:47:45 +0600

    main section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+   extend: {
+    colors: {
+      // Configure your color palette here
+      "igoo": "#0B132A",
+      "reddi": "#F53838",
+      "gray": "#6b7280",
+      "white": "#FFFFFF"
+    },
+    fontFamily:{
+      body: ["Rubik"]
+    },
+    lineHeight: {
+      'extra-loose': '90px',
+      'sm-loose': '24px'
+    },
+   },
   },
   plugins: [],
 }
